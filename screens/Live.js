e85771c772a1a18e606863d3f139b5849d195668
Baryan808 <onyobt50@gmail.com>
2023-07-21 22:48:54 +0200

    live

--- screens/Live.js
+++ screens/Live.js
@@ -1,65 +1,57 @@
-import React, { useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, PermissionsAndroid, Platform } from 'react-native';
-import RtcEngine, { ChannelProfile, ClientRole } from "react-native-agora";
+import React, { useEffect, useRef, useState } from 'react';
+import { Share, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import AgoraUIKit from 'agora-rn-uikit';
 
-async function requestCameraAndAudioPermission() {
-  try {
-    const granted = await PermissionsAndroid.requestMultiple([
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-    ]);
-    if (
-      granted["android.permission.RECORD_AUDIO"] === PermissionsAndroid.RESULTS.GRANTED &&
-      granted["android.permission.CAMERA"] === PermissionsAndroid.RESULTS.GRANTED
-    ) {
-      console.log("You can use the cameras & mic");
-    } else {
-      console.log("Permission denied");
+function Live({ route }) {
+
+  const [videoCall, setVideoCall] = useState(true);
+
+  const connectionData = {
+    appId: '35a8c9ce2ba04d5c9458575e4f7fc447',
+    channel: route.params.channel,
+  };
+
+  console.log(route)
+
+  const onShare = async () => {
+    try {
+      await Share.share({ message: route.params.channel });
+    } catch (error) {
+      console.log(error.message);
     }
-  } catch (err) {
-    console.warn(err);
-  }
-}
+  };
 
-export default function Live({ route }) {
+  const rtcCallbacks = {
+    EndCall: () => setVideoCall(false),
+  };
 
-  const isBroadCasting = route && route.params.type === "create"
-
-  const AgoraEngine = useRef();
-
-  const init = async () => {
-    AgoraEngine.current = await RtcEngine.create("35a8c9ce2ba04d5c9458575e4f7fc447");
-    AgoraEngine.current.enableVideo();
-    AgoraEngine.current.setChannelProfile(ChannelProfile.LiveBroadCasting);
-    if (isBroadCasting) AgoraEngine.current.setChannelProfile(ClientRole.Broadcaster);
-
-    AgoraEngine.current.addListenner('JoinChannelSuccess', (channel, uid, elapsed)=>{
-      console.log("Join success", channel, uid, elapsed)
-    })
-  }
-
-  useEffect(async () => {
-    const uid = isBroadCasting ? 1 : 0;
-    if (Platform.OS === 'android') await requestCameraAndAudioPermission();
-    init().then(()=>{
-      AgoraEngine.current.joinChannel(null, route && route.params.channel, null, uid);
-    })
-    return () => {
-      AgoraEngine.current.destroy();
-    }
-  }, [])
-
-  return (
-    <View style={styles.container}>
-      <Text>Live</Text>
-    </View>
+  return videoCall ? (
+    <>
+      <AgoraUIKit connectionData={connectionData} rtcCallbacks={rtcCallbacks} />
+      <TouchableOpacity style={styles.shareButton} onPress={onShare}>
+        <Text style={styles.shareButtonText}>Partager</Text>
+      </TouchableOpacity>
+    </>
+  ) : (
+    <Text onPress={() => setVideoCall(true)}>Démarrer</Text>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  shareButton: {
+    right: 0,
+    width: 80,
+    height: 40,
+    margin: 25,
+    borderRadius: 8,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#78b0ff",
   },
-});
+  shareButtonText: {
+    fontSize: 16,
+  },
+})
+
+export default Live
